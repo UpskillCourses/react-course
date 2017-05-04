a399eaebf0d72375741910e5a2cd8d13cddc2932
Richard Gill <richard@rgill.co.uk>
2017-05-04 20:47:03 +0000

    Lesson 8

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import RedditPost from './redditPost'
+import redditPosts from './data/redditPosts'
 
 class App extends Component {
   render() {
+    const redditPostComponents = redditPosts.map((redditPost) => {
+      return (
+        <RedditPost
+          key={redditPost.title}
+          title={redditPost.title} 
+          link={redditPost} 
+          commentCount={redditPost.commentCount} 
+          minutesAgo={redditPost.minutesAgo} 
+        />
+      )
+    })
     return (
       <div className="App">
-        <RedditPost title="Super fun dog!" link="http://google.com" commentCount={10} minutesAgo={2} />
-        <RedditPost title="Lots of fun cats" link="http://google.com" commentCount={5} minutesAgo={20} />
-        <RedditPost title="Awesome Fishes" link="http://google.com" commentCount={15} minutesAgo={30} />
-        <RedditPost title="New Solar Power Tech" link="http://google.com" commentCount={20} minutesAgo={10} />
-        
+        {redditPostComponents}
       </div>
     );
   }
---
+++ src/data/redditPosts.js
@@ -0,0 +1,6 @@
+export default [
+    {title: "Super fun dog!", link: "http://google.com", commentCount: 10, minutesAgo: 2},
+    {title: "Lots of fun cats", link: "http://google.com", commentCount: 5, minutesAgo: 20}, 
+    {title: "Awesome Fishes", link: "http://google.com", commentCount: 15, minutesAgo: 30},
+    {title: "New Solar Power, Tech", link: "http://google.com", commentCount: 20, minutesAgo: 10}
+]
