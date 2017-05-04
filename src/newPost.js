366918981bc5f753ff01f401533dfb15eda2ab16
Richard Gill <richard@rgill.co.uk>
2017-05-04 21:09:17 +0000

    Lession 10

--- src/newPost.js
+++ src/newPost.js
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Form from './core/form'
 
-export default () => <Form>
-    <label>Title</label>
-    <input />
-    <label>Link</label>
-    <input />
-    <label>Number of comments</label>
-    <input />
-    <label>Days ago</label>
-    <input />
-    
-    <input type="submit" value="Add new Post" />
-</Form>
+export default class NewPost extends Component {
+    constructor() {
+        super()
+        this.state = {
+            title: '',
+            link: '',
+            commentCount: 0,
+            daysAgo: 0,
+        }
+    }
+    render() {
+        
+        return (
+           <Form onSubmit={() => console.log('My state: ' + JSON.stringify(this.state))} >
+                <label>Title</label>
+                <input onChange={(event) => this.setState({title: event.target.value})} />
+                <label>Link</label>
+                <input onChange={(event) => this.setState({link: event.target.value})}/>
+                <label>Number of comments</label>
+                <input onChange={(event) => this.setState({commentCount: event.target.value})} />
+                <label>Days ago</label>
+                <input onChange={(event) => this.setState({daysAgo: event.target.value})}/>
+                
+                <input type="submit" value="Add new Post" />
+            </Form>
+        )
+ 
+    }
+
+}
