b1d30732d8f5bd4c1a98407134232225e746b05e
Richard Gill <richard@rgill.co.uk>
2017-05-04 19:36:31 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import RedditPost from './redditPost'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <RedditPost />
+        <RedditPost />
+        <RedditPost />
+        <RedditPost />
+        <RedditPost />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/redditPost.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default () => 
+    <div className="reddit-post">
+        <a href="https://google.com">Google</a>
+        <p>3 minutes ago</p>
+        <p>4 Comments</p>
+    </div>
