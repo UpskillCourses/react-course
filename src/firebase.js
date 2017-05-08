f67d2bd990632aadfe13018b53fd2cdcaf1286c4
Richard Gill <richard@rgill.co.uk>
2017-05-08 15:09:08 +0000

    Lesson 12

---
+++ src/firebase.js
@@ -0,0 +1,7 @@
+import firebase from 'firebase'
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDAt0HyazGAB3Dxb1luEStJX_37BLxZhwg",
+  authDomain: "reddit2-611a5.firebaseapp.com",
+  databaseURL: "https://reddit2-611a5.firebaseio.com",
+})
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { database } from 'firebase'
 import App from './App';
+import './firebase'
 import './index.css';
+
+database().ref('/').on('value', (snapshot) => console.log(snapshot.val()))
+
+database().ref('/').push({title: "New Solar Power, Tech", link: "http://google.com", commentCount: 20, minutesAgo: 10})
 
 ReactDOM.render(
   <App />,
